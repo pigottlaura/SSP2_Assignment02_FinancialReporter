d032c87765707b4317bfe9ee83c55fe51a04a506
Laura Pigott <pigottlaura@gmail.com>
2016-11-08 19:04:56 +0000

    Adding main control pages

--- wp-content/themes/financialreporter/single.php
+++ wp-content/themes/financialreporter/single.php
@@ -1,9 +1,9 @@
-<?php // Controls Blog Posts Page ?>
+<?php // Controls Individual Blog Posts ?>
 <?php get_header(); ?>
 
 <div class="row">
     <div class="col-xs-12">
-        blog
+        individual blog
         <?php // The Loop ?>
         <?php if(have_posts()) : while(have_posts()) : the_post(); ?>
             <h2><?php the_title(); ?></h2>
