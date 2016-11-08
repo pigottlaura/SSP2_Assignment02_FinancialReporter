66f98ebe971af6bea8ecbdfc4414e447f4acb5cb
Laura Pigott <pigottlaura@gmail.com>
2016-11-08 18:48:08 +0000

    Adding Bootstrap to the site

--- wp-content/themes/financialreporter/header.php
+++ wp-content/themes/financialreporter/header.php
@@ -2,20 +2,25 @@
     <head>
         <title>
             <?php
-                echo "Financial REporter";
-            ?>
-        </title>
-        <?php wp_head(); ?>
-    </head>
-    <body>
-        <nav>
-            <div>
-                <?php wp_nav_menu( array( 'menu' => 'header-menu')); ?>
-            </div>
-        </nav>
-
-        <h1>
-            <?php
                 echo "Financial Reporter";
             ?>
-        </h1>
+        </title>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+        <link rel="stylesheet" type="text/css" href="<?php echo get_bloginfo('template_url'); ?>/style.css">
+        <?php wp_head(); ?>
+    </head>
+    <body class="container-fluid">
+        <div class="row">
+            <div class="col-xs-3">
+                <h1>
+                    <?php
+                    echo "Financial Reporter";
+                    ?>
+                </h1>
+            </div>
+            <div class="col-xs-8">
+                <nav class="navbar navbar-default">
+                    <?php wp_nav_menu( array( 'menu' => 'header-menu')); ?>
+                </nav>
+            </div>
+        </div>
--- wp-content/themes/financialreporter/index.php
+++ wp-content/themes/financialreporter/index.php
@@ -1,7 +1,7 @@
 <?php get_header(); ?>
 
-<div>
-    <div>
+<div class="row">
+    <div class="col-xs-12">
         <?php // The Loop ?>
         <?php if(have_posts()) : while(have_posts()) : the_post(); ?>
             <h2><?php the_title(); ?></h2>
