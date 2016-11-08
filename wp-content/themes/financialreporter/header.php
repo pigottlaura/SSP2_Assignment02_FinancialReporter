b747486cd74a7efe3a3d10d26e747ef56233215e
Laura Pigott <pigottlaura@gmail.com>
2016-11-08 19:13:31 +0000

    Adding Favicon to the site

--- wp-content/themes/financialreporter/header.php
+++ wp-content/themes/financialreporter/header.php
@@ -5,6 +5,7 @@
                 echo "Financial Reporter";
             ?>
         </title>
+        <link rel="shortcut icon" href="<?php echo get_bloginfo('template_url'); ?>/favicon.ico" />
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
         <link rel="stylesheet" type="text/css" href="<?php echo get_bloginfo('template_url'); ?>/style.css">
         <?php wp_head(); ?>
