2c5528b339c80f5b3c7820e917949f553ed019c7
Laura Pigott <pigottlaura@gmail.com>
2016-11-24 12:24:35 +0000

    Recommenting Setup class

--- wp-content/themes/financialreporter/classes/lp_financialReporter_Setup.php
+++ wp-content/themes/financialreporter/classes/lp_financialReporter_Setup.php
@@ -11,38 +11,52 @@
             lp_financialReporter_Setup::addFilters();
         }
 
+        // Function which is invoked by the "after_switch_theme" action
+        // i.e. every time the theme is activated
         public static function activated() {
             // Checking that all database tables required by this theme exist
-            // (and if not, then creating them)
+            // (and if not, then creating them) using the DatabaseTables class
             lp_financialReporter_DatabaseTables::checkRequiredTables();
 
             // Checking that all pages required by this theme exist
-            // (and if not, then creating them)
+            // (and if not, then creating them) using the Pages class
             lp_financialReporter_Pages::checkRequiredPages();
         }
 
+        // Function which is invoked by te "switch_theme" action
+        // i.e. every time another theme is activated (this them is deactivated)
         public static function deactivated() {
+            // Removing all pages created by the theme, when the theme was activated
             lp_financialReporter_Pages::removeThemePages();
         }
 
+        // Adding action hooks every time a page is loaded, to detect actions such
+        //// as the theme being activated, deactivated or a user being deleted
         public static function addActions() {
-            // Adding the actions
+            // Invoking the activated() method of this class, every time this theme
+            // is activated (so that required tables and pages can be checked and/or created
             add_action("after_switch_theme", "lp_financialReporter_Setup::activated");
+
+            // Invoking the deactivated() method of this class, every time this theme
+            // is deactivated (i.e. another theme is activated) so that pages and options
+            // that were created when the theme was activated can be removed
             add_action("switch_theme", "lp_financialReporter_Setup::deactivated");
             add_action("delete_user", "lp_financialReporter_Setup::onDeleteUser");
         }
 
         public static function addFilters() {
-            // Creaing a filter, through which all uploads from this theme will be passed
-            // i.e. so all receipts can be uploaded to the "receipts" folder of the "uploads"
-            // directory
+            // Creaing a filter, through which all uploads from this theme will be passed,
+            // so that a custom directory can be specified, using the File classs i.e. so
+            // all receipts can be uploaded to the "receipts" folder of the "uploads" directory
             add_filter('upload_dir', 'lp_financialReporter_File::useCustomDir');
 
-            // Creating a filter, through which all uploads from this theme will be passed
-            // i.e. so that all filenames of receipts will be appended with the current
-            // timestamp (so as to avoid naming conflicts)
+            // Creating a filter, through which all uploads from this theme will be passed,
+            // so that the filename under which the file is stored can be customised i.e. so
+            // that all filenames of receipts will be appended with the current timestamp (so
+            // as to avoid naming conflicts)
             add_filter('wp_handle_upload_prefilter', 'lp_financialReporter_File::useCustomFilename' );
 
+            // Creating a filter for when pages
             add_filter("wp_page_menu_args", "lp_financialReporter_Pages::excludeFromMenu");
         }
 
